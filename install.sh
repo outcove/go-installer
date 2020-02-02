420c5d935b84aa51e4884cc957a562d51d35ad1e
outcove <44450330+outcove@users.noreply.github.com>
2020-02-02 15:31:26 -0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 GO_VERSION=1.13.7
 
-if [[ -f "/usr/local/go" ]]
+if [[ -d "/usr/local/go" ]]
 then
     echo "Go already installed."
     exit
@@ -14,7 +14,11 @@ wget https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz
 echo "Installing to /usr/local..."
 sudo tar -C /usr/local -xzf go$GO_VERSION.linux-amd64.tar.gz
 
-echo "Setting  Paths..."
-echo 'PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
-echo 'GOPATH=$HOME/go' >> ~/.bashrc
+if [[ $GOPATH != "" ]]
+then
+    echo "Setting  Paths..."
+    echo 'PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
+    echo 'GOPATH=$HOME/go' >> ~/.bashrc
+fi
+
 echo "...done."
