5b630091897173ef60325160fb1306b7658ae650
outcove <44450330+outcove@users.noreply.github.com>
2020-02-02 13:20:20 -0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+GO_VERSION=1.13.7
+
+echo hello!
+
+echo "Downloading Go Binary..."
+wget https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz
+
+echo "Installing to /usr/local..."
+tar -C /usr/local -xzf go$GO_VERSION.linux-amd64.tar.gz
+
+echo 'PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+
+echo 'GOPATH=$HOME/go' >> ~/.profile
