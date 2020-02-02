7297aabc20dfb2803fd8f4a43d28c97b7ecd0c2d
outcove <44450330+outcove@users.noreply.github.com>
2020-02-02 13:28:10 -0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,6 @@ wget https://dl.google.com/go/go$GO_VERSION.linux-amd64.tar.gz
 echo "Installing to /usr/local..."
 tar -C /usr/local -xzf go$GO_VERSION.linux-amd64.tar.gz
 
-echo 'PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+echo 'PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 
-echo 'GOPATH=$HOME/go' >> ~/.profile
+echo 'GOPATH=$HOME/go' >> ~/.bashrc
